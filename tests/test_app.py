df27eeb7aafc845e58637ad2c51ac57ffbed67cd
jad553 <jad553@drexel.edu>
2024-06-25 11:34:35 -0400

    Added assertions to confirm if everything was intialized correctly

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,17 +2,20 @@
 '''
     This file contains tests to test app.py
 '''
+import os
 import pytest
+import singleton
 from app import App
 
-def test_app_start(monkeypatch):
-    """Test that the REPL exits correctly on 'exit' command."""
-    monkeypatch.setattr('builtins.input', lambda _: 'exit')
+def test_app_start():
+    """Test that the app starts correctly and intailizes everything needed to run the app."""
     app = App()
     current_env = app.get_environment_variable('ENVIRONMENT')
     current_user = app.get_environment_variable('USERNAME')
     assert current_env in ['DEV', 'PROD'], f"Invalid ENVIRONMENT: {current_env}"
     assert current_user in ['jderik-local', 'jderik-dev', 'jderik-prod'], f"Invalid USERNAME: {current_user}"
+    assert os.path.exists(os.path.dirname(singleton.calc_history_path_location)), "Data directory was not created"
+    assert os.path.exists(singleton.calc_history_path_location), "Calculator History csv file was not created"
     with pytest.raises(SystemExit) as e:
         app.start()
     assert e.type == SystemExit
