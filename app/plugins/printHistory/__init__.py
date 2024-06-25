a162064af66970941a9521bbe82e6c68d19aebcf
jad553 <jad553@drexel.edu>
2024-06-25 17:22:25 -0400

    Created commands to manage CalculatorHistory

---
+++ app/plugins/printHistory/__init__.py
@@ -0,0 +1,24 @@
+# pylint: disable=unnecessary-dunder-call, invalid-name
+'''
+    Defines the REPL command to get and print the Calculator's History
+'''
+import logging
+from icecream import ic
+from app.commands import Command
+from app.calculator import CalculatorHistory
+
+class PrintHistoryCommand(Command):
+    """
+        This class extends the Abstract Command Class and gets and prints all calcualtions from the Calculator's History
+    """
+    def execute(self, user_input):
+        """
+            Gets and prints all calculations from the Calculator's History
+
+            @param user_input: not used by this method, added to adhere to Liskov substitution principle
+        """
+        logging.debug("Calculator History: %s", ic.format(CalculatorHistory.get_history()))
+        i = 1
+        for calc in CalculatorHistory.get_history():
+            print(f"{i}) {calc}")
+            i += 1
---
+++ app/plugins/saveHistory/__init__.py
@@ -0,0 +1,23 @@
+# pylint: disable=unnecessary-dunder-call, invalid-name
+'''
+    Defines the REPL command to Save the Calculator's History
+'''
+
+import logging
+from app.commands import Command
+from app.calculator import CalculatorHistory
+
+class SaveHistoryCommand(Command):
+    """
+        This class extends the Abstract Command Class and saves the history to the specified csv file
+    """
+
+    def execute(self, user_input):
+        """
+            Deletes all calculations from the Calculator's history
+
+            @param user_input: csv file to save the stored history to
+        """
+        CalculatorHistory.save_to_csv(user_input[0])
+        logging.debug("History Saved to %s", user_input[0])
+        print(f"History Saved to {user_input[0]}")
